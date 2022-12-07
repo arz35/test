a6fc184d3830cca141973d78dc555269c148cfd0
arz35 <113775553+arz35@users.noreply.github.com>
2022-12-08 05:17:02 +0700

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,28 +5,31 @@ import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 import navIcon4 from "../assets/img/nav-icon4.svg";
 import shape from '../assets/img/shape.jpg'
+import { Fade } from "react-reveal";
 
 export const Footer = () => {
   return (
     <>
-    <footer className="footer">
-      <div>
-      </div>
-        <Row className="align-items-center">
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
-              <a href="#"><img src={navIcon4} alt="Icon" /></a>
-            </div>
-            <p>Copyright 2022. All Rights Reserved</p>
-          </Col>
-        </Row>
-    </footer>
+    <Fade bottom cascade>
+      <footer className="footer">
+        <div>
+        </div>
+          <Row className="align-items-center">
+            <Col size={12} sm={6}>
+              <img src={logo} alt="Logo" />
+            </Col>
+            <Col size={12} sm={6} className="text-center text-sm-end">
+              <div className="social-icon">
+                <a href="#"><img src={navIcon1} alt="Icon" /></a>
+                <a href="#"><img src={navIcon2} alt="Icon" /></a>
+                <a href="#"><img src={navIcon3} alt="Icon" /></a>
+                <a href="#"><img src={navIcon4} alt="Icon" /></a>
+              </div>
+              <p>Copyright 2022. All Rights Reserved</p>
+            </Col>
+          </Row>
+      </footer>
+    </Fade>
     </>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,103 @@
 import React from 'react'
-import img35 from '../assets/img/Services-04.png'
+import img35 from '../assets/img/about.jpg'
 import { Fade } from 'react-reveal';
+import { Container,Row, Col } from 'react-bootstrap';
+import service01 from '../assets/img/Services-01.jpg';
+import service02 from '../assets/img/Services-02.jpg';
+import service03 from '../assets/img/Services-03.jpg';
+import service04 from '../assets/img/Services-04.jpg';
+import service05 from '../assets/img/Services-05.jpg';
+import service06 from '../assets/img/Services-06.jpg';
+
 
 export const Main = () => {
 
   return (
     <>
     <div className='bg-img'>
-      <h1>PT Solusi Industri Energi</h1>
-      <p>We provided Effective And Simple Solution For Your Industrial Needs</p>
+      <Fade left cascade>
+        <h1>PT Solusi Industri Energi</h1>
+        <p>We provided Effective And Simple Solution For Your Industrial Needs</p>
+      </Fade>
       <button>Company Profile</button>
     </div>
-    <div className='Wrapper'>
+    <div className='wrapper'>
       <img src={img35} alt="" className='img35'/>
-    </div>
-    <Fade right cascade>
-      <div className='about-layout'>
-          <p className="text-about">ABOUT US</p>
+      <Fade right cascade>
+        <div className='about-layout'>
+            <p className="text-about">ABOUT US</p>
 
-          <h1>PT Solusi Industri Energi</h1>
-          <br/>
-          <h5>Established on the basis of the growth of the construction and industrial sectors in Indonesia which experienced a surge in value that continues to rise. And the construction sector in Indonesia has grown 7-8 percent.</h5>
+            <h1>PT Solusi Industri Energi</h1>
             <br/>
-          <h5>Our commitment in every service Engineering procurement and construction is the satisfaction of partners or customers, to realize the commitment We provide quality products and a team of experienced experts and professionals in their fields.</h5>
-      </div>
+            <h5>Established on the basis of the growth of the construction and industrial sectors in Indonesia which experienced a surge in value that continues to rise. And the construction sector in Indonesia has grown 7-8 percent.</h5>
+              <br/>
+            <h5>Our commitment in every service Engineering procurement and construction is the satisfaction of partners or customers, to realize the commitment We provide quality products and a team of experienced experts and professionals in their fields.</h5>
+            <a href="#about">About Us</a>
+        </div>
+      </Fade>
+    </div>
+    <Fade bottom cascade>
+      <Container className='services-layout'>
+        <div className='text-services'>Our Services</div>
+        <h2 className='services-text'>What We do</h2>
+        <Row xs={1}>
+          <Col>
+            <div className='content-wrapper'>
+              <img src={service01} alt="No Internet Connection"/>
+              <div className='overlay'>
+                <div className='text-content'>Piping Fabrication & Installation</div>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className='content-wrapper'>
+              <img src={service02} alt="No Internet Connection"/>
+              <div className='overlay'>
+                <div className='text-content'>Mechanical & Installation</div>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className='content-wrapper'>
+              <img src={service03} alt="No Internet Connection"/>
+              <div className='overlay'>
+                <div className='text-content'>Heavy Lifting</div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container className='services-layout'>
+        <Row xs={1}>
+          <Col>
+            <div className='content-wrapper'>
+              <img src={service04} alt="No Internet Connection"/>
+              <div className='overlay'>
+                <div className='text-content'>Civil Engineering</div>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className='content-wrapper'>
+              <img src={service05} alt="No Internet Connection"/>
+              <div className='overlay'>
+                <div className='text-content'>Procurement Services & General Supplies</div>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className='content-wrapper'>
+              <img src={service06} alt="No Internet Connection"/>
+              <div className='overlay'>
+                <div className='text-content'>Special Services</div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+        <div className='detail'>
+          <a href='#services'>Go For Detail</a>
+        </div>
+      </Container>
     </Fade>
     </>
   )
